524bcb219007e6856e135b3411b7943c42fe39c8
GlendaSalas <glenda.salas.a@gmail.com>
2020-05-01 15:54:26 -0500

    Add vision and handlerbars for views engines to render templated responses

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 
 const Hapi = require('@hapi/hapi')
 const Path = require('path');
+const Vision = require('@hapi/vision');
+const Handlebars = require('handlebars');
 
 const init = async () => {
 
@@ -18,7 +20,22 @@ const init = async () => {
         method: 'GET',
         path: '/',
         handler: (request, h) => {
-            return 'Hello World!';
+            return h.view('index', {
+                name: 'Glenda Salas'
+            });
+        }
+    });
+    server.route({
+        method: 'GET',
+        path: '/tasks',
+        handler: (request, h) => {
+            return h.view('tasks', {
+                tasks: [
+                    { name: 'First task' },
+                    { name: 'Second task' },
+                    { name: 'Third task' },
+                ]
+            });
         }
     });
     await server.register(require('@hapi/inert'));
@@ -28,6 +45,11 @@ const init = async () => {
         handler: function (request, h) {
             return h.file('about.html');
         }
+    });
+    await server.register(Vision);
+    server.views({
+        engines: { html: Handlebars },
+        path: `${__dirname}/views`
     });
     await server.start();
     console.log('Server running on %s', server.info.uri);
